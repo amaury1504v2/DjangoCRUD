2a8089627fc56ba31c6898143b3218bcb7631861
Amaury Gimonnet <amaury.gimonnet@edu.devinci.fr>
2021-03-15 16:11:27 +0100

    Employee list

--- employee_register/forms.py
+++ employee_register/forms.py
@@ -5,4 +5,14 @@ class EmployeeForm(forms.ModelForm):
 
     class Meta:
         model = Employee
-        fields = '__all__'
+        fields = ('fullname', 
+        'emp_code', 'mobile', 'position')
+
+        labels = {'fullname': 'Full Name', 
+        'emp_code': 'EMP. Code'}
+
+    def __init__(self, *args, **kwargs):
+        super(EmployeeForm, self).__init__(*args, **kwargs) # *args: passer un argument sans mot clé 
+        # **kwargs: passer un argument avec mot clé
+        self.fields['position'].empty_label = "Select" # on change l'étiquette vide de la table position à Select
+        self.fields['emp_code'].required = False # le emp_code n'est pas initialement requis
--- employee_register/views.py
+++ employee_register/views.py
@@ -1,13 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import EmployeeForm
+from .models import Employee
 
 # Create your views here.
 def employee_list(request):
-    return render(request, "employee_register/employee_list.html")
+    context = {'employee_list':Employee.objects.all()} # on charge la liste d'employés via un contexte
+    return render(request, "employee_register/employee_list.html", context) # l'objet context est passé en tant que 3è paramètre
 
 def employee_form(request):
-    form= EmployeeForm()
-    return render(request, "employee_register/employee_form.html", {'form':form})
+    if request.method == "GET":
+        form= EmployeeForm()
+        return render(request, "employee_register/employee_form.html", {'form':form})
+    else:
+        form= EmployeeForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/employee/list')
 
 def employee_delete(request):
     return

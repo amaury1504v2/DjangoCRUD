cf38d88b58e660fc7312526d4564fed8660a9fbb
Amaury Gimonnet <amaury.gimonnet@edu.devinci.fr>
2021-03-16 18:49:03 +0100

    CRUD

--- employee_register/views.py
+++ employee_register/views.py
@@ -7,15 +7,25 @@ def employee_list(request):
     context = {'employee_list':Employee.objects.all()} # on charge la liste d'employés via un contexte
     return render(request, "employee_register/employee_list.html", context) # l'objet context est passé en tant que 3è paramètre
 
-def employee_form(request):
+def employee_form(request,id=0):
     if request.method == "GET":
-        form= EmployeeForm()
+        if id==0: 
+            form = EmployeeForm() 
+        else: 
+            employee = Employee.objects.get(pk=id) 
+            form = EmployeeForm(instance = employee) 
         return render(request, "employee_register/employee_form.html", {'form':form})
     else:
-        form= EmployeeForm(request.POST)
+        if id == 0: #if it is an insert operation
+            form = EmployeeForm(request.POST) #it will set an empty form
+        else: #if it is an update operation
+            employee = Employee.objects.get(pk=id) #it will search the form by id
+            form = EmployeeForm(request.POST,instance = employee) #populate the form with the corresponding record details
         if form.is_valid():
             form.save()
         return redirect('/employee/list')
 
-def employee_delete(request):
-    return
+def employee_delete(request,id):
+    employee = Employee.objects.get(pk=id)
+    employee.delete()
+    return redirect('/employee/list')
